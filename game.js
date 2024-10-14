4a5e0f4d8a9eaa4e98d0be4f3087dbfdc9831055
SammieDev <lythekiet333@gmail.com>
2024-10-14 18:18:24 +1030

    function start the game by pressing a key

--- game.js
+++ game.js
@@ -5,22 +5,40 @@ var randomChosenColor = buttonColors[nextSequence()];
 var gamePattern = [];
 // then add the randomChosenColor to the end of the gamePattern array
 gamePattern.push(randomChosenColor);
-
+// detect when any of the buttons are clicked
+var isPlaying = false;
+// keep track of the level
+var level = 0;
 $(".btn").click(function () {
   var userChosenColor = $(this).attr("id");
   // add the userChosenColor to the end of the userClickedPattern array
   userClickedPattern.push(userChosenColor);
-  console.log(userClickedPattern);
+  // animate the button that got pressed
+  animatePress(userChosenColor);
 });
+
+// press a keyboard key to start
+$(document).keypress(function () {
+  if (!isPlaying) {
+    isPlaying = true;
+    $("h1").text("Level " + level);
+    nextSequence();
+  }
+});
+
 // function to generate a random number between 0 and 3
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
+
+  level++;
   return randomNumber;
 }
+// function to play the sound of the button that got pressed
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
+// function to animate the button that got pressed
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
